60ec53e03a31518d1003667c2c53606d5b5d73e5
Lautaro  Marin <marin.lautaro.ibqm4b@gmail.com>
2025-07-06 19:35:21 -0300

    melkups primer commit

---
+++ src/components/Header/MenuMobile.tsx
@@ -0,0 +1,47 @@
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Button } from "../ui/button";
+import { menuLinks } from "@/utils/menuLinks";
+import { MdMenu } from "react-icons/md";
+
+const MenuMobile = () => {
+  return (
+    <div className="md:hidden">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button>
+            <MdMenu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+          </SheetHeader>
+          <ul className="flex flex-col p-4 gap-4">
+            {menuLinks.map((menu) => (
+              <li className="text-white w-full duration-300 hover:text-secondary">
+                <a href={menu.to}>{menu.label}</a>
+              </li>
+            ))}
+          </ul>
+          <SheetFooter>
+            <SheetClose asChild>
+              <Button variant="secondary" className="text-white!">
+                Cerrar
+              </Button>
+            </SheetClose>
+          </SheetFooter>
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
+
+export default MenuMobile;
---
+++ src/utils/menuLinks.ts
@@ -0,0 +1,14 @@
+export const menuLinks = [
+  {
+    label: "Nosotros",
+    to: "#nosotros",
+  },
+  {
+    label: "Materia Prima",
+    to: "#mPrima",
+  },
+  {
+    label: "Productos",
+    to: "#productos",
+  },
+];
